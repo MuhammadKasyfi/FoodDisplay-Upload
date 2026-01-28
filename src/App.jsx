5009fde428fe4087664fe9078043726a03515e91
MuhammadKasyfi <kasyfikh@gmail.com>
2026-01-28 09:28:41 +0800

    Revert "Initial upload multiple image"
    This reverts commit b80fc433dc102a7b53452206886ff466c7fe860d.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { use, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import React from 'react'
+import { useState } from 'react'
 import './App.css'
 import uploadIcon from './assets/upload.svg'
 
